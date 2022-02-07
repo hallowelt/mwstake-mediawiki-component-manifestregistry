e0386e544d9e5eba4e100569c0ae2a9ce3052cd9
rvogel <vogel@hallowelt.biz>
2022-02-07 16:36:21 +0100

    Bump version number

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_MANIFESTREGISTRY_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_MANIFESTREGISTRY_VERSION', '1.0.0' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_MANIFESTREGISTRY_VERSION', '2.0.1' );
 
 $GLOBALS['wgServiceWiringFiles'][] = __DIR__ . '/includes/ServiceWiring.php';
 
