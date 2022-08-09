f3816ec990492e364220a42e3e2b5859fb5b4475
jas <jontykantiwal@gmail.com>
2022-08-09 13:25:30 +0200

    Fixed phpcs action erros from test files

---
+++ extensions/Checklists/tests/phpunit/ChecklistsParserTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Checklists\Tests;
+
+use Checklists\Parser;
+use PHPUnit\Framework\TestCase;
+
+class ChecklistsParserTest extends TestCase {
+
+	protected function setUp() : void {
+		parent::setUp();
+	}
+
+	/**
+	 * @covers \Parser
+	 */
+	public function testUncheckedList() {
+		$testInput = "
+         
+
+
+        []Unchecked item 1
+        
+        
+        [] Unchecked item 2
+        
+        
+        [x] Checked item
+        ";
+		$outputHtml = "<ul><li>Unchecked item 1</li><li>Unchecked item 2</li>";
+		$outputHtml .= "<li><s>Checked item</s></li></ul>";
+		// Parser
+		$parser = new Parser();
+		$outputFromParser = $parser->parse( $testInput );
+		$this->assertTrue( $outputFromParser == $outputHtml, "Checklists parser is working fine!" );
+	}
+
+}
