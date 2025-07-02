6212b6f0221c9d7f8b5d9f48d0f5ac910cc2c72a
hslater <hamish.slater@hallowelt.com>
2025-07-02 21:13:45 +1200

    Update composer.json authors
    ERM42827

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_MANIFESTREGISTRY_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_MANIFESTREGISTRY_VERSION', '3.0.0' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_MANIFESTREGISTRY_VERSION', '3.0.1' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'manifestregistry', static function () {
